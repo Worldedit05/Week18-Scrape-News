ca2441141c68b992a1cf3bd3d6ab7b17d69904fb
Aaron Lee <worldedit05@gmail.com>
2016-12-17 03:24:54 -0600

    Added Heroku info

--- server.js
+++ server.js
@@ -124,6 +124,8 @@ app.post("/pictures/:id", function(req, res) {
   });
 });
 
-app.listen(3000, function() {
-  console.log("App is listening on port 3000");
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+  console.log("App is listening on " + PORT);
 });
